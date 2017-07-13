c74b6805c68782e1a74cc83aa85be20b43daace5
station3 <jhlee2@station3.co.kr>
2017-07-13 11:10:28 +0900

    Linked DataSource

--- test5/FirstViewController.swift
+++ test5/FirstViewController.swift
@@ -10,36 +10,48 @@ import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let list = ["Wake Up, Shower and Have Coffee", "Get to Work on Time" , "Work Out and Play Tennis Before Dinner"]
+    var list = ["Wake Up", "Get to Work" , "Work Out", "Dinner"]
     
+    @IBOutlet weak var myTableView: UITableView!
+    
+    /*
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+ 
+    */
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return (list.count)
+        return list.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = list[indexPath.row]
-        
-        return(cell)
+        return cell
     }
     
-    @IBOutlet weak var myTableView: UITableView!
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 

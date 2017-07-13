2ad901e929c07bd444b29482db5c90738c93de97
station3 <jhlee2@station3.co.kr>
2017-07-13 10:25:16 +0900

    Created 2 ViewController.Swift files & @IBOutlet

---
+++ test5/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test5
+//
+//  Created by station3 on 13/07/2017.
+//  Copyright © 2017 station3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
